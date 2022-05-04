6a7ee64aa981e9bc61e0ad1ebfadc790f707ec9d
Purpurova-k <katrin.purpurova@gmail.com>
2022-05-04 17:19:07 +0300

    Test added

--- test/unit/sortByName.test.js
+++ test/unit/sortByName.test.js
@@ -14,4 +14,18 @@ describe("Books names test suit", () => {
       "Гарри Поттер",
     ]);
   });
+
+  it("Books names of equal length", () => {
+    expect(
+      sorting.sortByName([
+        "Гарри Поттер",
+        "Гарри Поттер",
+        "Гарри Поттер",
+      ])
+    ).toEqual([
+      "Гарри Поттер",
+      "Гарри Поттер",
+      "Гарри Поттер",
+    ]);
+  });
 });
